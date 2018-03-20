ceee6846c623ee7c460c920b2ea1b845f941df57
lizzieavila <lizi_avila@yahoo.com.br>
2018-03-19 23:25:45 -0300

    commit

--- tipos variaveis.c
+++ tipos variaveis.c
@@ -7,12 +7,23 @@ void main(){
 
     int a;
     float b;
+    char c;
 
     a = 5;
     b = 2.5;
+    c = 'r';
 
     printf("\nO valor de a é: %d", a);
     printf("\nO valor de b é: %.2f", b);
+    printf("\nO valor de c é: %c", c);
+
+    scanf("%d",&a);
+    scanf("%f",&b);
+    scanf(" %c",&c);
+
+    printf("\nO valor de a é: %d", a);
+    printf("\nO valor de b é: %.2f", b);
+    printf("\nO valor de c é: %c", c);
 
     printf("\n");
     system("pause");
