473a56ee184fb60b5854f35af10db095b451ab34
lizzieavila <lizi_avila@yahoo.com.br>
2018-03-19 23:14:17 -0300

    commit

--- tipos variaveis.c
+++ tipos variaveis.c
@@ -4,4 +4,16 @@
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
+
+    int a;
+    float b;
+
+    a = 5;
+    b = 2.5;
+
+    printf("\nO valor de a é: %d", a);
+    printf("\nO valor de b é: %.2f", b);
+
+    printf("\n");
+    system("pause");
 }
